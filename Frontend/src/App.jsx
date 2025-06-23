cc24e443e450230bbada04d7106176ee343fd5ec
Luv <ir>
2025-06-23 23:03:54 +0530

    UI for whiteboard & Created Routes(frontend)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import './App.css'
+
 import Forms from './Components/Forms/Form'
+import Form from './Components/Forms/Form'
+import RoomPage from './Pages/RoomPage/RoomPage'
 
 const App = () => {
   return (
     <div className="container">
-      <Forms/>
+      <Routes>
+        <Route path="/" element={<Form/>} />
+        <Route path="/:roomId" element={<RoomPage/>} />
+      </Routes>
     </div>
   )
 }
---
+++ Frontend/src/Pages/RoomPage/RoomPage.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import "./RoomPage.css";
+import Whiteboard from "../../Components/Whiteboard/Whiteboard";
+
+const RoomPage = () => {
+  const [tool, setTool] = useState("pencil");
+  const [color, setColor] = useState("#000000");
+
+  return (
+    <div className="row">
+      <h1 className="text-center py-4">
+        White Board sharing App{" "}
+        <span className="text-primary">[Users Online : 0]</span>
+      </h1>
+      <div className="col-md-14 d-flex align-items-center justify-content-around">
+        <div className="d-flex mx-auto justify-content-between align-items-center gap-2">
+          <div className="col-md-2 d-flex gap-1">
+            <label htmlFor="pencil">Pencil</label>
+            <input
+              id="pencil"
+              type="radio"
+              name="tool"
+              value="pencil"
+              checked={tool === "pencil"}
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+
+          <div className="col-md-2 d-flex gap-1">
+            <label htmlFor="line">Line</label>
+            <input
+              id="line"
+              type="radio"
+              name="tool"
+              value="Line"
+              checked={tool === "Line"}
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+
+          <div className="col-md-2 d-flex gap-1">
+            <label htmlFor="rect">Rectangle</label>
+            <input
+              id="rect"
+              type="radio"
+              name="tool"
+              value="rect"
+              checked={tool === "rect"}
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="col-md-2">
+          <div className="d-flex align-items-center gap-2">
+            <label htmlFor="color">Select Color</label>
+            <input
+              id="color"
+              type="color"
+              className="mt-1"
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="col-md-2 d-flex gap-2">
+          <button className="btn btn-primary">Undo</button>
+          <button className="btn btn-outline-primary">Redo</button>
+        </div>
+
+        <div className="col-md-3">
+          <button className="btn btn-danger">Clear Canvas</button>
+        </div>
+      </div>
+
+      <div className="col-md-10 mx-auto mt-5 canvas-box">
+        <Whiteboard />
+      </div>
+    </div>
+  );
+};
+
+export default RoomPage;
